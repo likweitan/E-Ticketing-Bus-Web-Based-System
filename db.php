fb02035ec97d8530d1945a39c208eb0bc7550221
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-12 11:25:35 +0800

    added "loginheader.php"

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost"
-    $username = "root"
+    $servername = "localhost";
+    $username = "root";
     $password = "";
-    $databasename = "IBM4202_JAN2020_Project"
+    $databasename = "IBM4202_JAN2020_Project";
 
     if(mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
---
+++ loginheader.php
@@ -0,0 +1,10 @@
+<?php
+//PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
+session_start();
+
+if(!isset($_SESSION['username']))
+{
+    header("location:login.php");
+}
+
+?>
