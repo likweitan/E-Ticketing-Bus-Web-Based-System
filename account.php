b131024d520aad519e33428ee10aaff1125c9049
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-13 10:15:16 +0800

    changes on "loginheader.php"

---
+++ account.php
@@ -0,0 +1,3 @@
+<?php
+    echo basename($_SERVER['PHP_SELF']);
+?>
--- loginheader.php
+++ loginheader.php
@@ -2,11 +2,11 @@
 //PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
 session_start();
 
-if(!isset($_SESSION['username']))
+if(!isset($_SESSION['username']) && basename($_SERVER['PHP_SELF']) != "login.php")
 {
     header("location:login.php");
 }
-else
+else if(isset($_SESSION['username']) && basename($_SERVER['PHP_SELF']) != "index.php")
 {
     header("location:index.php");
 }
