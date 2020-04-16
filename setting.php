58191fb730f4907ab850abd6e3348d0349a48ba6
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-16 14:01:38 +0800

    added minecraft head

--- setting.php
+++ setting.php
@@ -76,7 +76,7 @@
 
     <main role="main" class="container">
       <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
-        <img class="mr-3" src="https://getbootstrap.com/assets/brand/bootstrap-outline.svg" alt="" width="48" height="48">
+        <img class="mr-3" src="https://www.mc-heads.net/avatar/1/100/nohelm.png" alt="" width="48" height="48">
         <div class="lh-100">
           <h6 class="mb-0 text-white lh-100"><?=$myFirstName." ".$myLastName?></h6>
           <small>Since <?=$sinceYear?></small>
