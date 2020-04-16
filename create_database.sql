d59d11875da75b21ac1aae06611645187cb4d909
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-16 20:21:25 +0800

    add insert into sql

--- create_database.sql
+++ create_database.sql
@@ -95,3 +95,11 @@ INSERT INTO `s900_database`.`account` (`FirstName`, `LastName`, `Email`, `Passwo
 
 
 INSERT INTO `s900_database`.`booking` (`BookingNo`, `AccountNo`, `Quantity`, `BusNo`, `BusSeat`, `BusDateTime`, `PromoCode`) VALUES ('1', '1', '1', 'JJP2930', '23', '2020-04-17 10:00:00', '1');
+
+INSERT INTO `s900_database`.`bus` (`BusNo`, `BusCompany`, `BusCapacity`) VALUES ('JQL2938', 'KKKL', '40');
+
+INSERT INTO `s900_database`.`bus_schedule` (`ScheduleNo`, `BusNo`, `ScheduleDepart`, `ScheduleArrive`, `ScheduleStartTime`, `ScheduleDuration`, `TicketPrice`) VALUES ('1', 'JQL2938', 'Johor Bahru', 'Kuala Lumpur', '10:00', '240', '38');
+
+INSERT INTO `s900_database`.`promo_code` (`PromoCode`, `PromoCodeDescription`, `PromoCodeEndTimestamp`, `PromoCodeStartTimestamp`) VALUES ('NEW2KL', 'New to KL!', '2020-04-30 00:00:00', '2020-04-16 00:00:00');
+
+INSERT INTO `s900_database`.`booking` (`BookingNo`, `AccountNo`, `PromoCode`, `ScheduleNo`, `Quantity`, `BusSeat`, `BusDateTime`) VALUES ('133123', '1', 'NEW2KL', '1', '1', '2', '2020-04-27 10:00:00');
