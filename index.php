7d73ff862aabc8ee81188fcfb860ca262b9dc98f
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-16 12:49:42 +0800

    updated logo

--- index.php
+++ index.php
@@ -21,8 +21,11 @@
   <body>
 
     <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">blueBus</a>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">
+    <img src="images/logo_white.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    
+  </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -41,23 +44,25 @@
             <?php
             if(isset($_SESSION['id']))
             {
-          echo '<form class="form-inline mt-2 mt-md-0"><div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    ';
-echo $myFirstName." ".$myLastName;
-  echo '</button>
-  <div class="dropdown-menu dropdown-menu-right">
-    <a class="dropdown-item" href="#">My Bookings</a>
-    <a class="dropdown-item" href="setting.php">Setting</a>
-    <a class="dropdown-item" href="logout.php">Logout</a>
-  </div>
-</div>
-</form>';
+          echo '<form class="form-inline"><div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+                echo $myFirstName." ".$myLastName;
+              echo '</a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="setting.php">Setting</a>
+                <a class="dropdown-item" href="logout.php">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div></form>';
             }
             else
             {
               echo '<form class="form-inline mt-2 mt-md-0" action="login.php">
-              <button type="submit" class="btn btn-secondary">Login</button>
+              <button type="submit" class="btn btn-outline-warning my-2 my-sm-0">Login</button>
               </form>';
             }
             ?>
