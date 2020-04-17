d91f65f13970e8797a1e39b18798e9af18de9447
Chong703 <chongyang703@gmail.com>
2020-04-17 23:29:23 +0800

    Update error message on password Sign Up

--- signup.inc.php
+++ signup.inc.php
@@ -3,6 +3,7 @@
 
     session_start();
     if(isset($_POST['submit'])){
+    $inputConfirmPassword = $_POST["inputConfirmPassword"];
     $inputFirstName = mysqli_real_escape_string($con, $_POST["inputFirstName"]);
     $inputLastName =  mysqli_real_escape_string($con,$_POST["inputLastName"]);
     $inputEmail =  mysqli_real_escape_string($con,$_POST["inputEmail"]);
@@ -10,9 +11,13 @@
     $inputGender = mysqli_real_escape_string($con, $_POST["inputGender"]);
     $inputDate =  mysqli_real_escape_string($con,$_POST["inputDate"]);
     $country =  mysqli_real_escape_string($con,$_POST["country"]);
+    $inputPhoneNumber = mysqli_real_escape_string($con,$_POST["inputPhoneNumber"]);
     
-
-    $insert =  mysqli_query($con,"INSERT INTO account (FirstName,LastName,Email,Password,Gender,BirthDate,Nationality)
-      VALUES('$inputFirstName', '$inputLastName', '$inputEmail', '$inputPassword', '$inputGender','$inputDate','$country')");
+    if($inputPassword==$inputConfirmPassword){
+    $insert =  mysqli_query($con,"INSERT INTO account (FirstName,LastName,Email,Password,Gender,BirthDate,Nationality, PhoneNumber)
+      VALUES('$inputFirstName', '$inputLastName', '$inputEmail', '$inputPassword', '$inputGender','$inputDate','$country','$inputPhoneNumber')");
     }
+    else
+    echo   "Confirm password invalid! Please match your password!";
+  }
 ?>
