6c8af730a8ad88f2b3b2dde447b8e958bcbcaa1b
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-18 11:11:23 +0800

    added admin panel

--- admin.php
+++ admin.php
@@ -267,7 +267,7 @@
     </script>
 
     <!-- Graphs -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script>
       var ctx = document.getElementById("myChart");
       var myChart = new Chart(ctx, {
@@ -275,7 +275,7 @@
         data: {
           labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
           datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+            data: [15339, 21345, 18483, 24003, 23489, 24092, 0],
             lineTension: 0,
             backgroundColor: 'transparent',
             borderColor: '#007bff',
