bd5b8ab1c296159b53f90deb3547f70e7629e72d
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-19 19:27:43 +0800

    fixed promotion page

--- assets/header.php
+++ assets/header.php
@@ -16,7 +16,7 @@
               <a class="nav-link" href="searchbus.php">Search Ticket</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="promotion,php">Promotions</a>
+              <a class="nav-link" href="promotion.php">Promotions</a>
             </li>
             <?php if(isset($_SESSION['id']))
             {
