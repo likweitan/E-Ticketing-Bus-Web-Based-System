0658b30b94aa4dc563060f28c53fd50c3416ac68
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-19 19:27:05 +0800

    updated header.php

--- setting.php
+++ setting.php
@@ -23,57 +23,10 @@
 
   <body class="bg-light">
 
-  <header>
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="index.php">
-    <img src="images/logo_white.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    
-  </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Promotions</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-            <?php
-            if(isset($_SESSION['id']))
-            {
-          echo '<form class="form-inline"><div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
-                echo $myFirstName." ".$myLastName;
-              echo '</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="setting.php">Setting</a>
-                <a class="dropdown-item" href="logout.php">Logout</a>
-              </div>
-            </li>
-          </ul>
-        </div></form>';
-            }
-            else
-            {
-              echo '<form class="form-inline mt-2 mt-md-0" action="login.php">
-              <button type="submit" class="btn btn-outline-warning my-2 my-sm-0">Login</button>
-              </form>';
-            }
-            ?>
-          
-        </div>
-        
-      </nav>
-    </header>
+  <!-- HEADER-->
+  <?php
+    include('assets/header.php');
+  ?>
 
     <main role="main" class="container">
     
