9bf03ee2c630a1320803b999529ea9d7a04c7bd2
elvisjaan0405 <57809604+elvisjaan0405@users.noreply.github.com>
2020-04-24 04:28:23 +0800

    Add scritp function in each management

---
+++ AdminPage/functionPromo.php
@@ -0,0 +1,87 @@
+<?php
+    
+    
+    include("../db.php");
+   
+    session_start();
+    $prCode = '';
+    $prcDes = '';
+    $prcEnd = '';
+    $prcStart = '';
+    $prcPer = '';
+    $update = false;
+    
+    if(isset($_POST['addProm']))
+	{
+        // get values 
+		$proCode = $_POST['pCode'];
+		$proCDes = $_POST['pcDes'];
+		$proEnd = $_POST['pcEnd'];
+        $proStart = $_POST['pcStart'];
+        $proPercentage = $_POST['pPer'];
+        $Sche = $_POST['Sch'];
+
+        
+        //Insert Query 
+        $query = "INSERT INTO promo_code(PromoCode, PromoCodeDescription, PromoPercentage, PromoCodeEndTimestamp,ScheduleNo,PromoCodeStartTimestamp)
+                  VALUES('$proCode', '$proCDes','$proPercentage', '$proEnd','$Sche','$proStart')";
+        //Past data information to database
+        if (!$result = mysqli_query($con,$query)) {
+            exit(mysqli_error($con));
+        }
+            $_SESSION['message'] = "Record has been Add";
+            $_session['msg_type'] = "warning";
+        
+            echo '<script language="javascript">';
+            echo 'alert("New Record Added successfully")';
+            echo '</script>';
+        
+        header("location: managePromo.php");
+    }
+
+    if(isset($_GET['edit']))
+	{
+        // get values 
+		$uproCode = $_GET['edit'];
+        $update = true;
+        $result = $con->query("SELECT * FROM promo_code WHERE PromoCode = '$uproCode'");
+        if (mysqli_num_rows($result)==1){
+            $row = $result->fetch_array();
+            $prCode = $row['PromoCode'];
+            $prcDes = $row['PromoCodeDescription'];
+            $prcPer = $row['PromoPercentage'];
+            $prcEnd = $row['PromoCodeEndTimestamp'];
+            $psch = $row['ScheduleNo'];
+            $prcStart = $row['PromoCodeStartTimestamp'];
+            
+        }  
+    }
+    
+    if(isset($_POST['updateProm']))
+    {
+        $proCode = $_POST['pCode'];
+        $proCDes = $_POST['pcDes'];
+        $proPer = $_POST['pPer'];
+		$proEnd = $_POST['pcEnd'];
+        $proStart = $_POST['pcStart'];
+        $prcSche = $_POST['Sch'];
+        $query = "UPDATE promo_code SET PromoCodeDescription = '$proCDes', 
+                    PromoPercentage = $proPer ,PromoCodeEndTimestamp = '$proEnd',
+                    PromoCodeStartTimestamp = '$proStart', ScheduleNo = '$prcSche'
+                     WHERE PromoCode = '$proCode'";
+        //Past data information to database
+        if (!$result = mysqli_query($con,$query)) {
+            exit(mysqli_error($con));
+        }
+       
+            $_SESSION['message'] = "Record has been Updated";
+            $_session['msg_type'] = "warning";
+        
+        header("location: managePromo.php");
+    }
+    if(isset($_POST['cancelUpdate']))
+	{
+        $update = false;
+        header("location: managePromo.php");
+    }
+?>
---
+++ AdminPage/functionUser.php
@@ -0,0 +1,63 @@
+<?php
+    
+    
+    include("../db.php");
+    session_start();
+
+    $accNo = '';
+    $accRole ='';
+    $fName ='';
+    $lName ='';
+    $ema ='';
+    $passw ='';
+    $gen ='';
+    $dobrith ='';
+    $nati ='';
+    $pnum ='';
+    $atstamp ='';
+    $update = false;
+
+    if(isset($_GET['edit']))
+	{
+        // get values 
+		$sAcc = $_GET['edit'];
+        $update = true;
+        $result = $con->query("SELECT * FROM account WHERE AccountNo = '$sAcc' AND AccountRole !='admin' ");
+        if (mysqli_num_rows($result)==1){
+            $row = $result->fetch_array();
+            $accNo = $row['AccountNo'];
+            $accRole =$row['AccountRole'];
+            $fName =$row['FirstName'];
+            $lName =$row['LastName'];
+            $ema =$row['Email'];
+            $passw =$row['Password'];
+            $gen =$row['Gender'];
+            $dobrith =$row['BirthDate'];
+            $nati =$row['Nationality'];
+            $pnum =$row['PhoneNumber'];
+            $atstamp =$row['AccountTimestamp'];
+        }  
+    }
+    if(isset($_POST['uUser']))
+    {
+        $uAccNo = $_POST['an'];
+        $uEmail =$_POST['em'];
+        $uPw =$_POST['pw'];
+        $uPhone =$_POST['pn'];
+        $query = "UPDATE account SET Email = '$uEmail', Password ='$uPw',
+                    PhoneNumber = $uPhone WHERE AccountNo = '$uAccNo'";
+        //Past data information to database
+        if (!$result = mysqli_query($con,$query)) {
+            exit(mysqli_error($con));
+            
+        }
+       
+            
+        header("location: manageUser.php");
+    }
+    if(isset($_POST['cUpate']))
+	{
+        $update = false;
+        header("location: manageUser.php");
+    }
+?>
