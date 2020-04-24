4d01e2c26856ea499c43f9a04b7bb5c7663f11e1
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-24 11:30:25 +0800

    modify file path

--- AdminPage/admin.php
+++ AdminPage/admin.php
@@ -89,10 +89,10 @@ break;
 
   <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">blueBus</a>
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="../index.php">blueBus</a>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="logout.php">Sign out</a>
+          <a class="nav-link" href="../logout.php">Sign out</a>
         </li>
       </ul>
     </nav>
