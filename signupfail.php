3581bc34ba9fa962f45078d212d24d9d643103e4
Chong703 <chongyang703@gmail.com>
2020-04-24 11:32:40 +0800

    delete signupfail,signupfail1

--- signupfail.php
+++
@@ -1,4 +0,0 @@
-<html>
-<p>Sorry... Email already taken.
- <a href="./signup.php">Click here back to Sign Up.</a></p>
-</html>
--- signupfail1.php
+++
@@ -1,4 +0,0 @@
-<html>
-<p>Confirm password invalid! Please match your password.
- <a href="./signup.php">Click here back to Sign Up.</a></p>
-</html>
