3d078aa66d16f553af83b06245f35b35f9b76aa1
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-24 14:20:22 +0800

    removed echo

--- payment.inc.php
+++ payment.inc.php
@@ -27,8 +27,6 @@
             $PaymentNo = $row['PaymentNo'];
         }
 
-        echo "INSERT INTO booking (BookingNo,AccountNo,PromoCode,ScheduleNo,Quantity,BusSeat,BusDateTime,BookingState,PaymentNo)
-        VALUES('$bookingno','$accountno','$promocode', '$scheduleno', '$quantity', '$seatno', '$busdatetime', '$bookingstate', '$PaymentNo')";
         $insertBooking =  mysqli_query($con,"INSERT INTO booking (BookingNo,AccountNo,PromoCode,ScheduleNo,Quantity,BusSeat,BusDateTime,BookingStatus,PaymentNo)
         VALUES('$bookingno','$accountno','$promocode', '$scheduleno', '$quantity', '$seatno', '$busdatetime', '$bookingstate', '$PaymentNo')");
 
