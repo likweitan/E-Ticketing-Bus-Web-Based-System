d4905c728987db069ec29b504fa43955eaf130c3
Lik Wei <likweitan@Liks-MacBook-Air.local>
2020-04-24 13:06:32 +0800

    modify loginheader

--- payment.inc.php
+++ payment.inc.php
@@ -1,6 +1,5 @@
 <?php
- require("db.php");
- session_start();
+ require("loginheader.php");
      if(isset($_GET['book_ticket'])){
         $PaymentType = mysqli_real_escape_string($con, $_POST["PaymentType"]);
         $CardName =  mysqli_real_escape_string($con,$_POST["CardName"]);
